ab1b407034f44f91ac3f85374f3597f73c3ca8e9
Marcus Henriksen <marcus.henriksen@live.no>
2023-01-01 18:04:56 +0100

    Scrolling and folder select
    - Scrolling now works with some bugs
    - Folder select is working, and file operations perform accordingly

--- scripts/gui_app.py
+++ scripts/gui_app.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import PhotoImage
-#import tkinter.messagebox
 import customtkinter
 from PIL import Image
 import glob
@@ -34,8 +33,10 @@ class App(customtkinter.CTk):
         super().__init__()
 
         #self.after(100, self.tickfunc)
+        self.current_folder = ""
 
-        self.title("File Cleanup")
+        self.title("File Organizer")
+
         # Why the hell does relative path not work??
         self.iconbitmap(os.path.join(path, "icon.ico"))
         
@@ -46,17 +47,45 @@ class App(customtkinter.CTk):
         posy = screen_size[1] // 2 - height // 2 - 50
 
         self.minsize(400, 400)
-        self.maxsize(screen_size[0]//5, screen_size[1] // 1.5)
-        self.geometry(f"{400}x{500}+{posx}+{posy}")
+        #self.maxsize(screen_size[0]//5, screen_size[1] // 2)
+        self.resizable(False, False)
+        self.geometry(f"{400}x{550}+{posx}+{posy}")
         #print(f"width:{screen_size[0]} height:{screen_size[1]}")
 
         # Top level grid
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        #self.grid_columnconfigure(0, weight=1)
+        #self.grid_rowconfigure(0, weight=1)
 
-        self.main = customtkinter.CTkFrame(self, corner_radius=0)
-        self.main.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
-        
+        main_frame = customtkinter.CTkFrame(self)
+        main_frame.pack(fill=tk.BOTH,expand=1)
+
+        # Create A Canvas
+        self.canvas = customtkinter.CTkCanvas(main_frame, highlightthickness=0, bg="#2b2b2b", height=100, yscrollincrement="0.3c")
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH,expand=True, padx=0, pady=0)
+
+        y_scrollbar = customtkinter.CTkScrollbar(main_frame, orientation=tk.VERTICAL, command=self.canvas.yview)
+        y_scrollbar.pack(side=tk.RIGHT, fill=tk.Y, padx=0, pady=0)
+
+        # Configure the canvas
+        self.canvas.configure(yscrollcommand=y_scrollbar.set)
+        self.canvas.bind("<Configure>",lambda e: self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))) 
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+        # Create Another Frame INSIDE the Canvas
+        second_frame = customtkinter.CTkFrame(self.canvas, corner_radius=0)
+
+        # Add that New Frame a Window In The Canvas
+        item = self.canvas.create_window((0,0),window= second_frame, anchor="nw")
+        print(self.canvas.winfo_width())
+        self.canvas.itemconfig(item, width=self.canvas.winfo_reqwidth()+30)
+        #self.canvas.itemconfig(item, height=self.canvas.winfo_reqheight())
+        #for thing in range(100):
+        #    customtkinter.CTkButton(second_frame ,text=f"Button  {thing}").grid(row=thing,column=0,pady=10,padx=10)
+        #return
+        #self.main = customtkinter.CTkFrame(second_frame, corner_radius=0)
+        #self.main.pack(fill=tk.BOTH, expand=True)
+        self.main = second_frame
+
         # Columns
         self.main.grid_columnconfigure(0, weight=1, minsize=10)
         self.main.grid_columnconfigure(1, weight=1, minsize=400)
@@ -78,8 +107,9 @@ class App(customtkinter.CTk):
         self.folderEntryContainer.pack()
 
         # Entry where used can paste a path, or entry will autofill according to file dialog popup
-        self.folderSelect = customtkinter.CTkEntry(self.folderEntryContainer, width=200)
+        self.folderSelect = customtkinter.CTkEntry(self.folderEntryContainer, width=250)
         self.folderSelect.pack(side=tk.LEFT)
+        self.folderSelect.bind("<KeyRelease>", self.check_folder)
 
         # Button for opening folder select dialog
         btnimg = customtkinter.CTkImage(
@@ -91,10 +121,6 @@ class App(customtkinter.CTk):
         self.folderSelectButton.configure(width=self.folderSelectButton.winfo_height())
         self.folderSelectButton.pack(side=tk.LEFT, padx=4)
 
-
-
-
-
         # Select search method label
         self.titleFrame = customtkinter.CTkFrame(self.main)
         self.titleFrame.grid(row=1, column=1, ipady=12, sticky="ew")
@@ -105,7 +131,7 @@ class App(customtkinter.CTk):
 
 
         # Search tab
-        self.fileTypeTab = customtkinter.CTkTabview(self.titleFrame, height=145, command=self.update_folder_info_manual)
+        self.fileTypeTab = customtkinter.CTkTabview(self.titleFrame, height=145, command=self.update_folder_info)
         self.fileTypeTab.grid(row=1, column=0, padx=12, pady=0, sticky="ew")
         self.fileTypeTab.add("Simple")
         self.fileTypeTab.add("Custom")
@@ -151,8 +177,6 @@ class App(customtkinter.CTk):
         self.filesFoundNumber2 = customtkinter.CTkLabel(self.filesFoundContainer2, text="0", fg_color="#424242", corner_radius=6)
         self.filesFoundNumber2.grid(row=0, column=1, padx=8, ipadx=4)
 
-
-
         # Container for mode (copy/move)
         self.modeFrame = customtkinter.CTkFrame(self.main, fg_color=self.main._fg_color)
         self.modeFrame.grid(row=3, column=1, pady=15)
@@ -192,23 +216,44 @@ class App(customtkinter.CTk):
         self.progressbar.pack()
         self.progressbar.set(0)
 
-        self.update_folder_info_manual()
+        #self.update_folder_info()
+
+        customtkinter.CTkLabel(self.main, text="").grid(row=6, column=1)
+
+        #for thing in range(6,100):
+        #    customtkinter.CTkButton(second_frame, text=f"Button  {thing}").grid(row=thing,column=1,pady=10,padx=10)
+
+    def _on_mousewheel(self, event):
+        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+
+    def check_folder(self, event=None):
+        directory = self.folderSelect.get()
+        if (os.path.exists(directory)):
+            self.folderSelect.configure(border_color="green")
+            self.current_folder = directory
+            self.update_folder_info()
+        else:
+            self.folderSelect.configure(border_color="red")
+        print(self.current_folder)
 
     def choose_folder(self):
-        pass
+        directory = tk.filedialog.askdirectory()
+        self.folderSelect.delete(0, tk.END)
+        self.folderSelect.insert(0, directory)
+        self.folderSelect.xview_scroll(1000, tk.UNITS)
+        self.folderSelect.focus()
+        self.check_folder()
 
-    def update_folder_info_manual(self):
+    def update_folder_info(self, event=None):
         "Update the label for showing what the folder name will be."
 
+        if self.current_folder == "": return
         foldername = self.get_folder_name()
         if foldername != "":
             self.folderNameLabel.configure(text=f"The folder name will be: {self.get_folder_name()}")
 
         self.filesFoundNumber.configure(text=str(self.get_num_files()))
         self.filesFoundNumber2.configure(text=str(self.get_num_files()))
-
-    def update_folder_info(self, event):
-        self.update_folder_info_manual()
 
     def get_ext_arr(self):
         "Get a string array off all the extensions the user has defined."
@@ -265,7 +310,7 @@ class App(customtkinter.CTk):
 
         # If the folder exists we add "_x" to the end where "x" is
         # the numbered copy, like "New folder (1)". So we get "fc_png_1", "fc_png_2"..
-        while os.path.isdir(os.path.join(path, retstr)):
+        while os.path.isdir(os.path.join(self.current_folder, retstr)):
             num = 1
             if len(retstr) > 3:
                 if retstr[len(retstr) - 2] == "_": # If we have already added to it
@@ -296,7 +341,7 @@ class App(customtkinter.CTk):
         return unique_elems
                 
     def make_folder(self, foldername):
-        folderpath = os.path.join(path, foldername)
+        folderpath = os.path.join(self.current_folder, foldername)
 
         try:
             os.mkdir(folderpath)
@@ -311,13 +356,13 @@ class App(customtkinter.CTk):
         self.progressContainer.grid_remove()
         self.progressbar.set(0)
         self.runBtn.configure(state="normal")
-        self.update_folder_info_manual()
+        self.update_folder_info()
 
     def get_num_files(self):
         ext_arr = self.get_ext_arr()
         file_count = 0
         for extension in ext_arr:
-            file_count = file_count + len(glob.glob(f"{path}\*.{extension}"))
+            file_count = file_count + len(glob.glob(f"{self.current_folder}\*.{extension}"))
         return file_count
 
     def run(self):
@@ -350,7 +395,7 @@ class App(customtkinter.CTk):
         print(ext_arr)
         original_file_count = 0
         for extension in ext_arr:
-            original_file_count = original_file_count + len(glob.glob(f"{path}\*.{extension}"))
+            original_file_count = original_file_count + len(glob.glob(f"{self.current_folder}\*.{extension}"))
 
         if original_file_count == 0:
             print("Amount of files found was zero. Returning.")
@@ -362,9 +407,9 @@ class App(customtkinter.CTk):
         if self.modeDropdown.get() == "Copy":
             for extension in ext_arr:
                 # Iterate through every file that has a matching extension
-                for file in glob.glob(f"{path}\*.{extension}"):
+                for file in glob.glob(f"{self.current_folder}\*.{extension}"):
 
-                    dst = os.path.join(path, foldername)
+                    dst = os.path.join(self.current_folder, foldername)
                     progress_float = ((original_file_count - remaining_files) / original_file_count)
 
                     try:
@@ -387,7 +432,7 @@ class App(customtkinter.CTk):
                 # Iterate through every file that has a matching extension
                 for file in glob.glob(f"{path}\*.{extension}"):
 
-                    dst = os.path.join(path, foldername)
+                    dst = os.path.join(self.current_folder, foldername)
                     progress_float = ((original_file_count - remaining_files) / original_file_count)
 
                     try:
@@ -408,8 +453,86 @@ class App(customtkinter.CTk):
         self.update()
         time.sleep(0.8)
         self.reset_fields()
-        # Finished
 
     def tickfunc(self):
         print(f"{self.winfo_geometry()}")
         self.after(100, self.tickfunc)
+
+
+class App2(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("File Cleanup")
+
+        # Why the hell does relative path not work??
+        self.iconbitmap(os.path.join(path, "icon.ico"))
+        
+        screen_size = gds.get_display_size()
+        width = screen_size[0] // 5
+        height = screen_size[1] // 1.5
+        posx = screen_size[0] // 2 - width // 2 - 50
+        posy = screen_size[1] // 2 - height // 2 - 50
+
+        self.minsize(400, 400)
+        self.maxsize(screen_size[0]//5, screen_size[1] // 1.5)
+        self.geometry(f"{400}x{500}+{posx}+{posy}")
+       
+        # main_frame = customtkinter.CTkFrame(self, fg_color="pink", corner_radius=0)
+        # main_frame.pack(fill=tk.BOTH, expand=True, padx=0, pady=0, ipadx=0, ipady=0)
+
+        # canvas = customtkinter.CTkCanvas(main_frame, highlightthickness=0, bg="lightblue")
+        # canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=20, ipadx=0, ipady=0)
+
+        # y_scrollbar = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=canvas.yview, bg="red")
+        # y_scrollbar.pack(side=tk.RIGHT,fill=tk.Y, padx=20)
+
+        # canvas.configure(yscrollcommand=y_scrollbar.set)
+        # canvas.bind("<Configure>",lambda e: canvas.configure(scrollregion=canvas.bbox(tk.ALL)))
+
+        # second_frame = customtkinter.CTkFrame(canvas, fg_color="green")
+
+        # # Add that New Frame a Window In The Canvas
+        # id = canvas.create_window((0,0), window=second_frame)
+
+        # #canvas.itemconfig(id, width=canvas.winfo_width())
+
+        # for thing in range(100):
+        #     customtkinter.CTkButton(second_frame, text=f"Button  {thing}").grid(row=thing,column=0,pady=10,padx=10)
+
+        main_frame = customtkinter.CTkFrame(self)
+        main_frame.pack(fill=tk.BOTH,expand=1)
+
+        # Create A Canvas
+        my_canvas = customtkinter.CTkCanvas(main_frame, highlightthickness=0, bg="#2b2b2b")
+        my_canvas.pack(side=tk.LEFT,fill=tk.BOTH,expand=1, padx=0, pady=0)
+
+        y_scrollbar = customtkinter.CTkScrollbar(main_frame,orientation=tk.VERTICAL,command=my_canvas.yview)
+        y_scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH, padx=0, pady=0)
+
+        # Configure the canvas
+        my_canvas.configure(yscrollcommand=y_scrollbar.set)
+        my_canvas.bind("<Configure>",lambda e: my_canvas.config(scrollregion= my_canvas.bbox(tk.ALL))) 
+
+        # Create Another Frame INSIDE the Canvas
+        second_frame = customtkinter.CTkFrame(my_canvas, corner_radius=0)
+
+        # Add that New Frame a Window In The Canvas
+        item = my_canvas.create_window((0,0),window= second_frame, anchor="nw")
+        print(my_canvas.winfo_reqwidth())
+        my_canvas.itemconfig(item, width=my_canvas.winfo_reqwidth())
+
+        for thing in range(100):
+            customtkinter.CTkButton(second_frame ,text=f"Button  {thing}").grid(row=thing,column=0,pady=10,padx=10)
+
+
+
+
+
+
+
+
+        
+
+
+
+
